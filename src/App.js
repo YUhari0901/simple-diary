65bd61f4cdeec397c6a7fe7ab565c31e9aa66220
YUhari0901 <lnara1004l@gmail.com>
2023-02-28 17:31:55 +0900

    초기값 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import Lifecycle from './Lifecycle';
+
+// https:jsonplaceholder.typicode.com/comments
 
 // const dummyList = [
 // {
@@ -41,6 +42,27 @@ function App() {
 
   const dataId = useRef (0);
 
+  const getData =async() =>{
+    const res = await fetch ("https://jsonplaceholder.typicode.com/comments").then((res) => res.json())
+    console.log(res);
+
+    const iniData = res.slice(0,20).map((it)=>{
+      return {
+        autor:it.email,
+        content:it.body,
+        emotion:Math.floor(Math.random()*5)+1,
+        created_date:new Date().getTime(),
+        id:dataId.current++ 
+      }
+    })
+    setData(iniData);
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+
   const onCreate = (autor,content,emotion) => {
     const created_date = new Date().getTime();
     const newItem ={
@@ -69,7 +91,6 @@ function App() {
   }
   return (
     <div className="App">
-    <Lifecycle/>
      <DiaryEditor onCreate={onCreate}/>
      <DiaryList diaryList ={data} onRemove={onRemove} onEdit={onEdit}/>
     </div>
